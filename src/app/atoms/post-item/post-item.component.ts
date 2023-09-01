ef13590e0f440b11b9fb399b08ed75d8c412704f
Casper <casperlay.akm@gmail.com>
2023-09-01 12:30:03 +0700

    add comment UI

--- src/app/atoms/post-item/post-item.component.ts
+++ src/app/atoms/post-item/post-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input,OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { PostService } from 'src/app/cores/services/post-service.ts.service';
 import { PostI } from 'src/app/models/post';
 import { POSTACTION } from 'src/app/models/post-action-type';
@@ -8,22 +8,28 @@ import { POSTACTION } from 'src/app/models/post-action-type';
   templateUrl: './post-item.component.html',
   styleUrls: ['./post-item.component.scss']
 })
-export class PostItemComponent implements OnInit{
+export class PostItemComponent implements OnInit {
   slideConfig = { slidesToShow: 1, slidesToScroll: 1, autoplay: false, arrows: false };
   type = POSTACTION;
   @Input() postContent: PostI;
+  @Output() addComment = new EventEmitter<boolean>()
 
   constructor(private postService: PostService) { }
 
   ngOnInit() {
     // this.toggleReaction(this.type: POSTACTION,event: Event);
   }
-  toggleReaction(type: POSTACTION,event :Event) {
+
+  toggleReaction(type: POSTACTION, event: Event) {
     event.preventDefault();
-    this.postService[`${type}Posts`](this.postContent).subscribe((res)=>{
+    this.postService[`${type}Posts`](this.postContent).subscribe((res) => {
       console.log(res);
-      
+
     })
+  }
+
+  openComment() {
+    this.addComment.emit(true)
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { APP_CONFIG } from './app/models/app-config';
+
+fetchAppConfig().then((configJson: any) => {
+  
+  platformBrowserDynamic([{ provide: APP_CONFIG, useValue: configJson }]).bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+})
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+
+async function fetchAppConfig(): Promise<any> {
+  const stage = environment.stage
+  if(stage === 'development') {
+    const response = await fetch("/assets/" + stage + ".json")
+    return Promise.resolve(await response.json())
+  }
+  
+}
